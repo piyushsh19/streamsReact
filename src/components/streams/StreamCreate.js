d31a2c1021ea75818e57fd4db221b380dae7bc07
piyush-sh19 <piyush.sh19@gmail.com>
2020-06-28 19:11:23 +0530

    commit

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,67 @@
 import  React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
-};
+class StreamCreate extends React.Component{
+    
+    renderError({ error, touched }) {
+        if (touched && error) {
+            return(
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
 
-export default StreamCreate;
+    /*
+    renderInput(formProps){
+        return (
+            <input
+                onChange={formProps.input.onChange}
+                value={formProps.input.value}
+            />
+        );
+    }
+    */
+   //destruct
+   renderInput = ( {input, label, meta} ) => {
+    return (
+        <div className="field">
+            <label>{label}</label>
+            <input {...input} autoComplete="off" />
+            <div>{this.renderError(meta)}</div>
+        </div>
+        );
+    }
+    
+    onSubmit(formValues) {
+        console.log(formValues);
+    }
+
+    render() {
+        return(
+            // we have to enter classname as error so that it so that semantic ui does not hide msg usig css
+            <form onSubmit= {this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" component={this.renderInput} label="Enter Title"/>
+                <Field name="description" component={this.renderInput} label= "Enter Description"/>
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
+}
+
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = "You must enter title";
+    }
+    if(!formValues.description) {
+        errors.description = "you must enter a description";
+    }
+    return errors;
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
